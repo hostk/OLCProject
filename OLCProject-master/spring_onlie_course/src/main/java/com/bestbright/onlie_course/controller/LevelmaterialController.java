f72f35feefde5dda89edf327f4105539eb308675
swe zin nway <meeswezin2020@gmail.com>
2020-09-25 14:26:00 +0630

    add

--- OLCProject-master/spring_onlie_course/src/main/java/com/bestbright/onlie_course/controller/LevelmaterialController.java
+++ OLCProject-master/spring_onlie_course/src/main/java/com/bestbright/onlie_course/controller/LevelmaterialController.java
@@ -7,28 +7,31 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.bestbright.onlie_course.dto.Level_materialDTO;
 import com.bestbright.onlie_course.model.Level_material;
 
 import com.bestbright.onlie_course.repository.LevelMaterialRepository;
 import com.bestbright.onlie_course.repository.LevelRepository;
+import com.bestbright.onlie_course.service.Level_materialService;
 @Controller
 public class LevelmaterialController {
 	@Autowired
 	private LevelRepository levelRepository;
 	@Autowired
 	private LevelMaterialRepository levelMaterialRepository;
-	
+	@Autowired
+	private Level_materialService level_materialservice;
 	
 	@GetMapping("/create_levelmaterial")
 	public String createLevelMaterial(Model model) {
-		model.addAttribute("levelM", new Level_material());
+		model.addAttribute("levelM", new Level_materialDTO());
 		model.addAttribute("levelList",levelRepository.findAll());
 		return "add_levelmaterial";
 	}
 	@PostMapping("/save_levelmaterial")
-	public String saveLevelmaterial(@ModelAttribute("levelM")Level_material level_material,Model model) {
+	public String saveLevelmaterial(@ModelAttribute("levelM")Level_materialDTO level_materialdto,Model model) throws Exception {
 		
-		levelMaterialRepository.save(level_material);
+		level_materialservice.saveLevelmaterial(level_materialdto);
 		
 		model.addAttribute("levelmaterialList",levelMaterialRepository.findAll());
 		
