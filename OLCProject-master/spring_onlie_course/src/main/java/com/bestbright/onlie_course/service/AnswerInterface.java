2988e3018232c8d2defe34536c2c2f66a7c9f525
swe zin nway <meeswezin2020@gmail.com>
2020-10-17 21:41:48 +0630

    add

--- OLCProject-master/spring_onlie_course/src/main/java/com/bestbright/onlie_course/service/AnswerInterface.java
+++
@@ -1,9 +0,0 @@
-package com.bestbright.onlie_course.service;
-
-import com.bestbright.onlie_course.dto.AnswerDTO;
-
-public interface AnswerInterface {
-
-	void saveAnswer(AnswerDTO answerdto);
-	
-}
--- OLCProject-master/spring_onlie_course/src/main/java/com/bestbright/onlie_course/service/QuestionInterface.java
+++
@@ -1,8 +0,0 @@
-package com.bestbright.onlie_course.service;
-
-import com.bestbright.onlie_course.dto.QuestionDTO;
-
-public interface QuestionInterface {
-
-	void saveQuestion(QuestionDTO questiondto);
-}
